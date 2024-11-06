e47f937bd37157aa47cf8e0f5654a7279db3d17c
greg <a@b.c>
2024-11-06 17:16:54 +0100

    Gestion du placement des éléments + Design des éléments + Logo App

--- app/src/main/java/fr/cpe/genial_degre/MainActivity.java
+++ app/src/main/java/fr/cpe/genial_degre/MainActivity.java
@@ -1,10 +1,18 @@
 package fr.cpe.genial_degre;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,17 +21,45 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Liaison avec le Button
-        Button bt = findViewById(R.id.bouton);
+        // Changer la couleur de la barre de navigation et de la barre d'état
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setNavigationBarColor(Color.parseColor("#fcefdd")); // Couleur de la barre de navigation
+            getWindow().setStatusBarColor(Color.parseColor("#fcefdd")); // Couleur de la barre d'état
+        }
 
-        // Action lorsque le bouton est cliqué
+        Button bt = findViewById(R.id.bouton);
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                // Lancer l'activité ServeurConfigurationActivity
                 Intent intent = new Intent(MainActivity.this, ServeurConfigurationActivity.class);
                 startActivity(intent);
             }
         });
+
+        List<String> items = new ArrayList<>();
+        Collections.addAll(items, "Lux", "Infra Rouge", "UV", "Temp", "Humidité", "Pression");
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        Adapter adapter = new Adapter(items);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP | ItemTouchHelper.DOWN, 0) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                int fromPosition = viewHolder.getAdapterPosition();
+                int toPosition = target.getAdapterPosition();
+                Collections.swap(items, fromPosition, toPosition);
+                adapter.notifyItemMoved(fromPosition, toPosition);
+                return true;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                // Non utilisé
+            }
+        });
+
+        itemTouchHelper.attachToRecyclerView(recyclerView);
     }
 }
